d6a9e73569b0a5a658e9b597659b69a516099d0a
yug1307 <161486463+yug1307@users.noreply.github.com>
2025-06-13 15:35:09 +0530

    Update Topbar.jsx

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -20,9 +20,9 @@ const Topbar = () => {
   
         {/* Menu Items */} 
         <ul className={`flex-col items-center justify-center md:flex-row md:flex md:items-center md:space-x-2 md:space-y-0 absolute md:static right-0 top-12 bg-teal-800 md:bg-transparent w-full md:w-auto z-50 transition-all duration-300 ${isOpen ? 'flex' : 'hidden'}`}>
-          <Link to="/"><div className="block px-4 py-2 hover:text-yellow-300">Home</div></Link>
-          <Link to="/nonveg"><div className="block px-4 py-2 hover:text-yellow-300">Non - Veg</div></Link>
-          <Link to="/veg"><div className="block px-4 py-2 hover:text-yellow-300">Veg</div></Link>
+          <Link to="/"><div className="block px-4 py-2 hover:text-purple-300">Home</div></Link>
+          <Link to="/nonveg"><div className="block px-4 py-2 hover:text-purple-300">Non - Veg</div></Link>
+          <Link to="/veg"><div className="block px-4 py-2 hover:text-purple-300">Veg</div></Link>
         </ul>
       </div>
     </div>
